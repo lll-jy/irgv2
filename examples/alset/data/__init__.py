51f191cf142baa60a0bdec4db600ee492c822f46
pengiyi <cjpcctvwc@gmail.com>
2022-10-10 15:17:37 +0800

    new table

--- examples/alset/data/__init__.py
+++ examples/alset/data/__init__.py
@@ -1,7 +1,7 @@
 """Table data processing."""
 
 from .sis import personal_data, sis_academic_career, sis_academic_program, sis_academic_program_offer, sis_plan_offer, \
-    sis_academic_plan, sis_enrolment
+    sis_academic_plan, sis_enrolment, gym, sis_module_enrolment, sis_course, sis_credits, sis_milestone
 
 __all__ = (
     'personal_data',
